1e73a81ff08280c53504f40ca6b483b4608fe838
Nuria A. Malet Quintar <nmaletq.pr@gmail.com>
2022-05-03 21:37:56 -0300

    changed to run on H2 database

--- webapp/src/main/java/main/SpringSecurityConfig.java
+++ webapp/src/main/java/main/SpringSecurityConfig.java
@@ -9,37 +9,36 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import main.service.UserDetailsServiceImplement;
 
 @Configuration
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private BCryptPasswordEncoder passswordEncoder;
-	
-	@Autowired
-	private UserDetailsServiceImplement userDetailsService;
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/home","/css/**","/js/**","/img/**").permitAll()
-			.antMatchers("/new-course").hasAnyRole("ADMIN")
-			.antMatchers("/professors").hasAnyRole("ADMIN")
-			.antMatchers("/subjects").hasAnyRole("ADMIN")
-			.antMatchers("/edit-professor").hasAnyRole("ADMIN")
-			.antMatchers("/courses").hasAnyRole("ADMIN","STUDENT")
-			.antMatchers("/subject-view").hasAnyRole("STUDENT")
-			.anyRequest().authenticated()
-			.and()
-			.formLogin().loginPage("/login").permitAll()
-			.and()
-			.logout().permitAll()
-			.and()
-			.exceptionHandling().accessDeniedPage("/error_403");
-	}
-	
 
 	@Autowired
-	public void ConfigurerGlobal(AuthenticationManagerBuilder build) throws Exception {	
-		build.userDetailsService(userDetailsService)
-			.passwordEncoder(passswordEncoder);
+	private UserDetailsServiceImplement userDetailsService;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/home", "/css/**", "/js/**", "/img/**", "/h2-console/**").permitAll()
+				.antMatchers("/new-course").hasAnyRole("ADMIN")
+				.antMatchers("/professors").hasAnyRole("ADMIN")
+				.antMatchers("/subjects").hasAnyRole("ADMIN")
+				.antMatchers("/edit-professor").hasAnyRole("ADMIN")
+				.antMatchers("/courses").hasAnyRole("ADMIN", "STUDENT")
+				.antMatchers("/subject-view").hasAnyRole("STUDENT")
+				.anyRequest().authenticated()
+				.and()
+				.formLogin().loginPage("/login").permitAll()
+				.and()
+				.logout().permitAll()
+				.and()
+				.exceptionHandling().accessDeniedPage("/error_403");
 	}
-	
+
+	@Autowired
+	public void ConfigurerGlobal(AuthenticationManagerBuilder build) throws Exception {
+		build.userDetailsService(userDetailsService)
+				.passwordEncoder(passswordEncoder);
+	}
+
 }
